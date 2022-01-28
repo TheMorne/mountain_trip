2ed7a65f0c4c771359688287891cbf2e52040311
TheMorne <mornef.dt@gmail.com>
2022-01-28 13:27:51 +0200

    Clean up

--- lib/cubits/app_cubit_logics.dart
+++ lib/cubits/app_cubit_logics.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_cubit/cubits/app_cubit.dart';
 import 'package:flutter_cubit/pages/detail_page.dart';
-import 'package:flutter_cubit/pages/home_page.dart';
 import 'package:flutter_cubit/pages/welcome_page.dart';
 
 import '../pages/navpages/main_page.dart';
